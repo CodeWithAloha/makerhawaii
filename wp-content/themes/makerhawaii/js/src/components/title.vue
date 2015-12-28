e7189e02a3ee31c0aa57cc060505a23605fec9de
Russell Vea <rvea@atlanticmedia.com>
2015-12-27 16:53:43 -0800

    Title vue component.

---
+++ wp-content/themes/makerhawaii/js/src/components/title.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <nav class="header__nav"><a class="header__title" href="/">Maker Hawaii</a></nav>
+  </header>
+</template>
+
+<style>
+  @import "normalize.css";
+  @import "colors.css/myth/variables.css";
+  @import '../styles/config/structure.css';
+
+  .header {
+    z-index: 1;
+    position: fixed 0 0 auto 0;
+    background: var(--green);
+    padding: 8px 10px;
+    text-align: center;
+    @nest &__title {
+      font-size: 32px;
+      color: white;
+      text-decoration: none;
+    }
+  }
+</style>
+
+
+<script>
+  export default {}
+</script>
