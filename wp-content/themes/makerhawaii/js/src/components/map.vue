4607a92597443468e510c41a2963ad6d0f443552
Russell Vea <russellbvea@gmail.com>
2016-06-04 11:10:59 -1000

    [#8]

--- wp-content/themes/makerhawaii/js/src/components/map.vue
+++ wp-content/themes/makerhawaii/js/src/components/map.vue
@@ -1,148 +1,57 @@
 <script>
+  import MapboxClient from 'mapbox';
   import {MAPBOX_API_KEY} from './config/mapbox.js';
-
+  import mapbox from 'mapbox.js';
   import {MAKER_STORE} from '../stores/maker_store.js';
+  import Vue from 'vue';
 
   export default {
     data() {
       return {
         markers: {},
-        spaces: {},
-        active: false,
-        markers_added: false,
-        map_loaded: false
-      }
-    },
-    methods: {
-      add_markers(val, markers) {
-        if(!this.$data.map_loaded) {
-          return
-        }
-
-        Object.assign(this.$data, {markers_added: true})
-        val.forEach((space) => {
-
-          const { lng, lat} = space.location
-
-          markers.features =
-            markers.features.concat([
-              {
-                type: "Feature",
-                geometry: {
-                  type: "Point",
-                  coordinates: [lng, lat]
-                },
-                properties: {
-                  ID: space.ID
-                }
-              }
-            ])
-
-          Object.assign(this.$data.markers, {
-            [space.ID]: [lng, lat]
-          });
-
-          Object.assign(this.$data.spaces, {
-            [space.ID]: space
-          })
-        });
+        active: false
       }
     },
     attached() {
       // instantiate map client
-      require(['mapbox-gl/dist/mapbox-gl.js'], (mapbox) => {
-        const {
-          Map,
-          Navigation
-        } = mapbox;
-        mapbox.accessToken = MAPBOX_API_KEY;
+      L.mapbox.accessToken = MAPBOX_API_KEY;
+      this.map = L.mapbox.map(
+        this.$el, 'mapbox.streets');
 
-        const center = [-157.371200, 20.865516]
-        this.map = new Map({
-          container: this.$el,
-          center,
-          zoom: 6,
-          style: 'mapbox://styles/russellvea2/cihib8squ00tdjnkx9bwq8tvt'
-        });
+      const HAWAII_CENTER = [21.013688, -157.537787];
+      this.map.setView(HAWAII_CENTER, 6);
 
-        const markers = {
-          type: "FeatureCollection",
-          features: []
-        }
-
-        this.map.addControl(new Navigation());
-
-        this.map.on('load', () => {
-
-          Object.assign(this.$data, {map_loaded: true})
-
-          if(this.$data.markers_added === false) {
-            const makerspaces =
-              MAKER_STORE
-                .reference('makerspaces')
-                .cursor()
-                .deref()
-            this.add_markers(makerspaces, markers)
-          }
-
-          this.map.addSource("markers", {
-            type: "geojson",
-            data: markers
-          })
-
-          this.map.addLayer({
-            id: "markers",
-            interactive: true,
-            type: "symbol",
-            source: "markers",
-            layout: {
-              "icon-image": "star-15"
-            }
-          })
-        });
-
-        this.map.on('click', (e) => {
-          this.map.featuresAt(e.point, {
-            layer: 'markers',
-            radius: 10,
-            includeGeometry: true
-          }, (err, features) => {
-
-            if(err) {
-              throw err;
-            }
-
-            if(features.length === 0) {
-              return;
-            }
-
-            const space = this.$data.spaces[features[0].properties.ID];
+      // add markers when they're instantiated
+      MAKER_STORE.reference('makerspaces').observe((val) => {
+        val.forEach((space) => {
+          let {lat, lng} = space.location;
+          let marker = new L.marker([lat,lng]);
+          marker.addTo(this.map);
+          marker.on('click', (e) => {
 
             MAKER_STORE
               .cursor('scrolled_space')
-              .update(() => space);
+              .update(() => space)
 
             MAKER_STORE
               .cursor('active_space')
-              .update(() => space);
+              .update(() => space)
+
           })
+          Object.assign(this.$data.markers, {
+            [space.ID]: marker
+          });
         });
+      });
 
-        // change map view
-        MAKER_STORE
-          .reference('active_space')
-          .observe((val) => {
-            if(!val) {
-              Object.assign(this.$data, {active: false});
-              return
-            }
-
-            Object.assign(this.$data, {active: true});
-            let latlng = this.$data.markers[val.ID];
-            this.map.setZoom(13);
-            this.map.panTo(latlng);
-        });
-      })
+      // change map view
+      MAKER_STORE.reference('active_space').observe((val) => {
+        if(val) {
+          Object.assign(this.$data, {active: true});
+        }
+        let marker = this.$data.markers[val.ID];
+        this.map.setView(marker._latlng, 13);
+      });
     },
   }
 </script>
@@ -170,7 +79,7 @@
       position: fixed auto 0 0 0;
       @media screen and (min-width: 64em) {
         position: fixed 0 auto auto 0;
-        height: 100%;
+        height: calc(100vh - 50px);
         width: 50%;
       }
       z-index: 2;
@@ -179,5 +88,4 @@
     }
   }
 </style>
-
-<style src="mapbox-gl/dist/mapbox-gl.css"></style>
+<style src="mapbox.js/theme/style.css"></style>
